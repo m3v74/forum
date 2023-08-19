0d14756672bbc9439673b74ee9ca0beaf2418ab9
Mo <mef74rus@gmail.com>
2023-08-19 21:16:30 +0400

    Добавлен роут очистки кэша

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,15 @@
 <?php
 
+use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\Route;
 
+Route::get('/clear', function () {
+    Artisan::call('cache:clear');
+    Artisan::call('route:clear');
+    Artisan::call('view:clear');
+    Artisan::call('route:cache');
+    Artisan::call('config:cache');
+
+    return "Cache cleared successfully";
+});
 Route::get('/', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
